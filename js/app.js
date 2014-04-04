b2ebc7de6ecaaa009481d60772ea250625e55e8f
duner <asduner@gmail.com>
2014-04-04 16:47:10 -0500

    widths update on click

--- js/app.js
+++ js/app.js
@@ -5,12 +5,33 @@ function Image(url) {
 }
 
 $(document).ready(function() {
+	
+	var wrapper = $('div.wrapper');
+	
+	$('div.wrapper').append("<div class='image left'></div>");
+	$('div.wrapper').append("<div class='image right'></div>");
+	
+	leftImage = $('div.image.left')
+	rightImage = $('div.image.right')
 
-	function createBASlider() {
-		$('div.wrapper').append("<div class='image left'></div>");
-		$('div.wrapper').append("<div class='image right'></div>");
+	leftImage.width('50%');
+	rightImage.width('50%');
+
+	wrapper.click(function(e) { updateSlider(e) });
+
+	function updateSlider(e) {
+		var offset = wrapper.offset();
+		var width = wrapper.width();
+		var relX = e.pageX - offset.left;
+
+		var leftPercent = (relX / width) * 100 + "%";
+		var rightPercent = (100 - (relX / width) * 100) + "%";
+
+		leftImage.width(leftPercent);
+		rightImage.width(rightPercent);
+
 	}
 
-	createBASlider();
+	//createBASlider();
 
 });
