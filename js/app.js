b11960dc5d09d43c203c82ed6f182a96f50e080d
duner <asduner@gmail.com>
2014-04-04 22:38:25 -0500

    ImageSlider and Graphc classes both work

--- js/app.js
+++ js/app.js
@@ -1,33 +1,45 @@
 
 (function (document, window) {
 
-	var imageSlider = function(element, images, options) {
+	var imageSlider = function(id, images, options) {
+	
+		function setBackgroundImage(element, url) {
+			var property = "url(" + url + ")";
+			element.css("background-image", property);
+		}
 
-		function Image(a) {
-			this.url = a.url;
+		function Graphic(a) {
+			this.imgSrc = a.imgSrc;
 			this.title = a.title;
+			this.credit = a.credit
 		};
 
-		function ImageSlider(elementID, images, options) {
+		function ImageSlider(id, images, options) {
 			var i;
 
 			if (images.length == 2) {
-				this.images = images;
-				console.log(images[1].url);
+				this.imgBefore = new Graphic(images[0]);
+				this.imgAfter = new Graphic(images[1]);
 			} else {
 				throw new Error("The images paramater takes two Image objects.");
 			}
 
 			this.options = {
 				animate: true,
-				transition: 400
+				transition: 400,
+				showDates: true
 			};
 
 			for (i in options) {
 				this.options[i] = options[i];
 			}
 
-			this.wrapper = $('#' + elementID);
+			this.wrapper = $('#' + id);
+			this.wrapper.append("<div class='image left'></div>");
+			this.wrapper.append("<div class='image right'></div>");
+			
+			this.leftImage = $('div.image.left')
+			this.rightImage = $('div.image.right')
 
 			this._init(this);
 		};
@@ -35,19 +47,17 @@
 		ImageSlider.prototype = {
 
 			_init: function() {
-				this.wrapper.append("<div class='image left'></div>");
-				this.wrapper.append("<div class='image right'></div>");
-				
-				this.leftImage = $('div.image.left')
-				this.rightImage = $('div.image.right')
-
 				this.leftImage.width('50%');
 				this.rightImage.width('50%');
+
+				setBackgroundImage(this.leftImage, this.imgBefore.imgSrc)
+				setBackgroundImage(this.rightImage, this.imgAfter.imgSrc)
+
 			}
 
 		};
 
-		return new ImageSlider(element, images, options);
+		return new ImageSlider(id, images, options);
 	};
 	
 	window.imageSlider = imageSlider;
