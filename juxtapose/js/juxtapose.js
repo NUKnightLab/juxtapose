7d45ec4c0246643753d234082dfb65e6415ba89d
Alex Duner <alex.duner@nytimes.com>
2015-08-04 15:33:38 -0400

    Should fix #61 but should probably test it

--- juxtapose/js/juxtapose.js
+++ juxtapose/js/juxtapose.js
@@ -529,11 +529,13 @@
       this.slider.addEventListener("touchstart", function(e) {
         e = e || window.event;
         e.preventDefault();
+        e.stopPropagation();
         self.updateSlider(e, true);
 
         this.addEventListener("touchmove", function(e) {
           e = e || window.event;
           e.preventDefault();
+          e.stopPropagation();
           self.updateSlider(event, false);
         });
 
